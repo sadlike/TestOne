6201aba2e18784ff2bb4b6c6a6d5aab7137d3649
王文鹏 <ioswang1989@163.com>
2016-11-18 17:22:47 +0800

    sf

--- TestOne/ViewController.m
+++ TestOne/ViewController.m
@@ -271,7 +271,7 @@ void MyDrawColoredPattern (void *info,CGContextRef content)
         });
         
     });
-    //12 同是下载
+    //12 同是下载 f sdf sdf sdf sf sdf sd fsd fsd fs
     
     dispatch_group_notify(group, main_queue, ^{
         
